bcd8203dee089d9a1e740788d28ccf2066f81dc4
Under <jorgeolmo.I@gmail.com>
2022-03-24 14:13:32 +0100

    Privera version con tailwind

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>Texto de ejemplo para mostrar el navbar</h1>
+  <h1 class="text-5xl">Texto de ejemplo para mostrar el navbar</h1>
+  <button class="bg-sky-500/100">Testing</button>
 </template>
 
 <script>
