30f73dd036094e23d9e3c46cc436fa41f7ec2549
Under <jorgeolmo.I@gmail.com>
2022-03-24 15:10:05 +0100

    Arreglo inicialización firebase

--- src/main.js
+++ src/main.js
@@ -6,4 +6,31 @@ import store from './store'
 // Importacion de estilos
 import './assets/tailwind.css'
 
+// Importación de firebase
+// Importacion de firebase
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore/lite"
+import { getAuth } from "firebase/auth"
+import firebase from "firebase/compat/app"
+import "firebase/compat/storage"
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_APIKEY,
+    authDomain: process.env.VUE_APP_AUTHDOMAIN,
+    projectId: process.env.VUE_APP_PROJECTID,
+    storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+    appId: process.env.VUE_APP_APPID,
+    measurementId: process.env.VUE_APP_MESEASUREDMENTID
+}
+
+firebase.initializeApp(firebaseConfig)
+var storage = firebase.storage()
+// Inicialización firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth()
+
 createApp(App).use(store).use(router).mount('#app')
+
+export { db, auth, storage }
