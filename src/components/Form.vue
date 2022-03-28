f987e75ff4e58ef317ef9ca04d9cdca2d8153ed9
Under <jorgeolmo.I@gmail.com>
2022-03-28 19:43:35 +0200

    Primera versión de formulario

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,4 @@
 <template lang="">
-    <h1>Texto de ejemplo para agregar modelos</h1>
     <!-- component -->
     <div class="p-5">
         <!-- Stepper -->
@@ -49,32 +48,73 @@
         <!-- Primer seccion -->
         <div class="mt-8 p-4" v-if="step == 0">
             <div>
-                <div class="font-bold text-gray-600 text-xs leading-8 uppercase h-6 mx-2 mt-3">Full Name</div>
                 <div class="flex flex-col md:flex-row">
-                    <div class="w-full flex-1 mx-2 svelte-1l8159u">
-                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                            <input placeholder="First Name" class="p-1 px-2 appearance-none outline-none w-full text-gray-800"> </div>
+                    <div class="w-full flex-1 mx-2 ">
+                        <div class="font-bold text-black text-xs leading-8 uppercase h-6 mx-2 mt-3">Marca</div>
+                        <div class=" my-2 p-1 flex rounded border relative">
+                            <svg class="absolute right-2 top-2 pointer-events-none h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                            </svg>
+                            <select class="p-1 px-2 appearance-none outline-none w-full text-gray-800 form-select" ref="marca" @change="getModelos()">
+                                <option disabled selected value="0">Seleccione una marca</option>
+                                <option v-for="marca in marcas" :value="marca">{{marca}}</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="w-full flex-1 mx-2 svelte-1l8159u">
-                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
-                            <input placeholder="Last Name" class="p-1 px-2 appearance-none outline-none w-full text-gray-800"> </div>
+                    <div class="w-full flex-1 mx-2 ">
+                        <div class="font-bold text-black text-xs leading-8 uppercase h-6 mx-2 mt-3">Modelo</div>
+                        <div class=" my-2 p-1 flex rounded border relative">
+                            <svg class="absolute right-2 top-2 pointer-events-none h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                            </svg>
+                            <select class="p-1 px-2 appearance-none outline-none w-full text-gray-800 form-select" ref="modelo" @change="getModelos()">
+                                <template v-if="modelos.length == 0">
+                                    <option disabled selected value="0">Seleccione una marca primero</option>
+                                </template>
+                                <template v-else>
+                                    <option disabled selected value="0">Seleccione un modelo</option>
+                                    <option v-for="modelo in modelos" :value="modelo" :key="modelo">{{modelo}}</option> 
+                                </template>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="w-full flex-1 mx-2 ">
+                        <div class="font-bold text-black text-xs leading-8 uppercase h-6 mx-2 mt-3">Versión concreta</div>
+                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded ">
+                            <input v-if="modelos.length != 0" placeholder="First Name" class="p-1 px-2 appearance-none outline-none w-full text-gray-800"> 
+                            <input v-else placeholder="First Name" class="p-1 px-2 appearance-none outline-none w-full text-gray-800" disabled> 
+                        </div>
                     </div>
                 </div>
                 <div class="flex flex-col md:flex-row">
-                    <div class="w-full mx-2 flex-1 svelte-1l8159u">
+                    <div class="w-full mx-2 flex-1 ">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Username</div>
-                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
+                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded ">
                             <input placeholder="Just a hint.." class="p-1 px-2 appearance-none outline-none w-full text-gray-800"> </div>
                     </div>
-                    <div class="w-full mx-2 flex-1 svelte-1l8159u">
+                    <div class="w-full mx-2 flex-1 ">
                         <div class="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Your Email</div>
-                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
+                        <div class="bg-white my-2 p-1 flex border border-gray-200 rounded ">
                             <input placeholder="jhon@doe.com" class="p-1 px-2 appearance-none outline-none w-full text-gray-800"> </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="mt-8 p-4" v-if="step == 1">
+            <h1>Datos avanzados</h1>
+        </div>
+        <div class="mt-8 p-4" v-if="step == 2">
+            <h1>Imagenes</h1>
+        </div>
+        <div class="mt-8 p-4" v-if="step == 3">
+            <h1>Resumen</h1>
+        </div>
+        <div class="mt-8 p-4">
             <div class="flex p-2 mt-4">
                 <button @click="previus()" class="text-base hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer hover:bg-gray-200  bg-gray-100 text-gray-700 border duration-200 ease-in-out border-gray-600 transition">Paso anterior</button>
+                <div class="flex-auto flex flex-row-reverse">
+                    <button @click="setCar()" class="text-base  ml-2  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer hover:bg-teal-600  bg-teal-600 text-teal-100 border duration-200 ease-in-out border-teal-600 transition">Comprobar campos</button>
+                </div>
                 <div class="flex-auto flex flex-row-reverse">
                     <button @click="next()" class="text-base  ml-2  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer hover:bg-teal-600  bg-teal-600 text-teal-100 border duration-200 ease-in-out border-teal-600 transition">Paso siguiente</button>
                 </div>
@@ -86,15 +126,31 @@
 <script>
     export default {
         data: () => ({
-            step: 0
+            step: 0,
+            marcas: [],
+            modelos: [],
         }),
         methods: {
             next () {
                 if (this.step < 3) this.step++
             },
             previus () {
-               if (this.step != 0) this.step--
+                if (this.step != 0) this.step--
+            },
+            setCar () {
+                var car = {
+                    marca: this.$refs.marca.value
+                }
+
+                console.log(car);
+            },
+            getModelos () {
+                this.modelos = ['modelo 1', 'modelo 2', 'modelo 3'];
+                console.log('test');
             }
+        },
+        mounted () {
+            this.marcas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         }
     }
 </script>
